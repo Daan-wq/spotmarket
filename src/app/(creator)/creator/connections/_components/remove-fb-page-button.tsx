9f9267182b813b1530a45e41b611490d8504301e
Daan Tuinman <daan0529@icloud.com>
2026-05-16 22:10:51 +0200

    Refine disconnect menu hover state

--- src/app/(creator)/creator/connections/_components/remove-fb-page-button.tsx
+++ src/app/(creator)/creator/connections/_components/remove-fb-page-button.tsx
@@ -13,7 +13,6 @@ export function RemoveFbPageButton({
   return (
     <DisconnectButton
       label={label}
-      platform="Facebook"
       onConfirm={async () => {
         await removeFbPage(connectionId);
       }}
